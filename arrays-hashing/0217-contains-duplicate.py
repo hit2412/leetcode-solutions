7dd3a3f190f036247bc1b0eede3c0607699e9319
Hitha V B <hithashree.vb@gmail.com>
2026-09-17 13:16:30 +1000

    Refactor containsDuplicate with hash set approach
    Commented out the brute force approach and added a hash set approach for better performance.

--- arrays-hashing/0217-contains-duplicate.py
+++ arrays-hashing/0217-contains-duplicate.py
@@ -1,10 +1,24 @@
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
-        n=len(nums)
-        for i in range(n):
-            for j in range(i+1,n):
-                if nums[i]==nums[j]:
-                    return True
+    # Approach 1: Brute force
+    # Time: O(n^2) and Space: O(1)
+    # Result: TLE (too slow for n up to 10^5)
+
+        # n=len(nums)
+        # for i in range(n):
+        #     for j in range(i+1,n):
+        #         if nums[i]==nums[j]:
+        #             return True
+        # return False
+
+    # Approach 2: Hash set
+    # Time: O(n) and Space: O(n)
+
+        check = set()
+        for num in nums:
+            if num in check:
+                return True
+            check.add(num)
         return False
 
         
