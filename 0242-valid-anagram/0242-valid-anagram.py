694192f077c2f5967b5d82dab14ba3c685a08ba3
Hitha V B <hithashree.vb@gmail.com>
2026-09-22 10:33:13 +1000

    Time: 19 ms (16%), Space: 20.3 MB (17.59%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,15 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
-            return False
-        lists=list(s)
+        return sorted(s)==sorted(t)
+    #Brute force : Time: O(n^2) and Space: O(n)
+        # if len(s)!=len(t):
+        #     return False
+        # lists=list(s)
 
-        for ch in t:
-            if ch in lists:
-                lists.remove(ch)
-            else:
-                return False
-        return True
+        # for ch in t:
+        #     if ch in lists:
+        #         lists.remove(ch)
+        #     else:
+        #         return False
+        # return True
+
+    
 
 
         
